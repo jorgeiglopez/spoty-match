b030600f074fd96c93e089a9b045f427d7cf9b92
Jorge Lopez <jorge.lopez@fluentcommerce.com>
2020-05-23 00:52:01 +1000

    First commit

---
+++ src/test/java/com/jorgeiglopez/spotymatch/SpotyMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgeiglopez.spotymatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotyMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
